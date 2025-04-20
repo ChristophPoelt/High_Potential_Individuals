62f05712e9610fb9f935ec2fd3587319b8ccb767
Christoph Pölt <c.poelt@campus.lmu.de>
2025-04-20 13:17:50 +0200

    final version of the 2D evolutionary algorithm with hpi

---
+++ src/IndividualWithFPF.java
@@ -0,0 +1,15 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+
+@Getter
+public class IndividualWithFPF {
+    private final double x1;
+    private final double x2;
+    private final double fpfValue;
+
+    public IndividualWithFPF(Individual individual, double fpfValue) {
+        this.x1 = individual.getX1();
+        this.x2 = individual.getX2();
+        this.fpfValue = fpfValue;
+    }
+}
